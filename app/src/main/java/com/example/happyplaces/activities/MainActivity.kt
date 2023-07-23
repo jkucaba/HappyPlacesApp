0e9c9acb822ad685993cf63ae94a4eef3ebc1691
Jakub <jakub.kucaba@gmail.com>
2023-07-23 12:21:24 +0200

    adding roundeed images

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplaces.R
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,16 +30,29 @@ class MainActivity : AppCompatActivity() {
         }
         getHappyPlacesListFromLocalDB()
     }
+    /**
+     * A function to populate the recyclerview to the UI.
+     */
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
+
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rv_happy_places_list.adapter = placesAdapter
+    }
 
     private fun getHappyPlacesListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if(getHappyPlaceList.size > 0){
-            for(i in getHappyPlaceList){
-                Log.e("Title,", i.title)
-                Log.e("Description,", i.description)
-            }
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_added.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_added.visibility = View.VISIBLE
         }
     }
 }
